3a972bc823536d4965174e04cfb13d7c2d7c053d
이은재 <>
2024-02-16 11:49:07 +0900

    test

---
+++ src/main/java/org/zerock/githubpractice2/GitHubPractice2Application.java
@@ -0,0 +1,13 @@
+package org.zerock.githubpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubPractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/githubpractice2/GitHubPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.githubpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
